61b2c3f2ab557cf3d88bd0ad653500a3c89de99e
= <=>
2021-09-12 21:51:13 +0900

    フォトライブラリ_雛形作成

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,400;1,500&family=Mukta&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../public/css/style.css">
+  <title>Photos</title>
+</head>
+<body>
+  <header>
+    <h1>My Photos</h1>
+  </header>
+
+  <div class="container">
+    <div id="photo_unit">
+      <div class="photo">
+        <img src="../public/images/parakeet_1.jpg">
+        <div class="inner">
+          <p class="comment">コメント</p>
+          <span class="nickname">ニックネーム</span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
