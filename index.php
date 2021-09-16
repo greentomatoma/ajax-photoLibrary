2ea4e98f680b579c15755c72ab4f245a2594f533
= <greentomatoma@gmail.com>
2021-09-16 11:42:52 +0900

    jsファイル_swoper部分切り離し

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
     </div>
   </div>
   <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
-  <script src="../public/css/hero-slider.js"></script>
+  <script src="../public/css/modules/hero-slider.js"></script>
   <script src="../public/css/main.js"></script>
 </body>
 </html>
